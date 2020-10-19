fc83455d50469e47ee3607839f0f1333646e3ea0
Eddiexian <43458446+Eddiexian@users.noreply.github.com>
2020-10-19 14:19:57 +0800

    Update model.py

--- app/model.py
+++ app/model.py
@@ -10,7 +10,7 @@ import numpy as np
 model = load_model('app/model/mnist_model.h5')
 
 def predict(input):
-    input = np.expand_dims(input, axis=0)
+    #input = np.expand_dims(input, axis=0)
     result = model.predict(input)
     num = (np.where(result==np.max(result[0]))[1])[0]
     print(num)
