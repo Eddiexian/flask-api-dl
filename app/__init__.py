771ee2fd494193de8ee3aa3652eed4e27e03e4fb
Eddiexian <43458446+Eddiexian@users.noreply.github.com>
2020-10-19 14:34:28 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -25,7 +25,7 @@ def postInput():
     #x1 = eval(x1)
     print(x1)
     input = np.array(x1)
-    input = input.reshape(1, 1, 28, 28)
+    input = input.reshape(1, 28, 28, 1)
     result = model.predict(input)
     
     return jsonify({'return': str(result)})
